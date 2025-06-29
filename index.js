780941adb022ddc21bf30d15e42e5d45b30cdff8
LHetet <lillian@hetetschoolofmaoriart.com>
2025-06-29 20:39:34 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-const express = require("express");
-const redirects = require("./public/kaiawhina-redirects.json");
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get("/:ref", (req, res) => {
-  const ref = req.params.ref.toLowerCase();
-
-  const match = redirects.kaiawhina.find(k =>
-    k.name.toLowerCase().replace(/\s+/g, '') === ref
-  );
-console.log(ref, match);
-
-  if (match) {
-    return res.redirect(301, match.referral);
-  } else {
-    return res.status(404).send("Kaiawhina not found");
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`Redirect server running on port ${PORT}`);
-});
+{
+  "kaiawhina": [
+    {
+      "slug": "admin",
+      "name": "Admin Campaign",
+      "referral": "https://www.themaoriweavingexperience.com/kaiawhina-invitation"
+    },
+    {
+      "slug": "aroharangi",
+      "name": "Aroha Rangi",
+      "referral": "https://www.themaoriweavingexperience.com/kaiawhina-invitation"
+    }
+  ]
+}
